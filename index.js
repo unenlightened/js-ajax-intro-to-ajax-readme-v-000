c98da37684521d2f21173595644cfc7b86ed07ae
Chely Ho <adept_inept@yahoo.com>
2018-07-11 00:21:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ function showRepositories(event, data) {
   document.getElementById("repositories").innerHTML = repoList
 }
 
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories)
