3432816ab3e29150634da1b012a3a5450133728a
Chely Ho <adept_inept@yahoo.com>
2018-07-11 00:15:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function showRepositories(event, data) {
   //this is set to the XMLHttpRequest object that fired the event
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  
+
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
