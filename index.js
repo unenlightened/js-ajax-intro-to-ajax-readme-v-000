c24b48be14737c7956ae809c562c6e95c8ad00c0
Chely Ho <adept_inept@yahoo.com>
2018-07-11 00:09:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function showRepositories(event, data) {
 
 function getRepositories() {
   const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
 }
